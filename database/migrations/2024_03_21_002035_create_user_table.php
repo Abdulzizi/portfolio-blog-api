7b28f5b65da5d69eff2b03b7169affb4c4139f63
Abdul Jawad Azizi <jawadazizi052@gmail.com>
2025-04-29 06:38:50 +0700

    Init api

--- database/migrations/2024_03_21_002035_create_user_table.php
+++ database/migrations/2024_03_21_002035_create_user_table.php
@@ -13,34 +13,24 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('m_user', function (Blueprint $table) {
+        Schema::create('user', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->uuid('m_user_roles_id')->comment('Fill with id from table m_user_roles');
-            $table->string('name', 100)
+
+            $table->string('username', 100)
                 ->comment('Fill with name of user');
             $table->string('email', 50)
                 ->comment('Fill with user email for login');
             $table->string('password', 255)
                 ->comment('Fill with user password');
-            $table->string('phone_number', 25)
-                ->default(null)
-                ->comment('Fill with phone number of user')
-                ->nullable();
-            $table->string('photo', 100)
-                ->comment('Fill with user profile picture')
-                ->nullable();
+
             $table->timestamp('updated_security')
                 ->comment('Fill with timestamp when user update password / email')
                 ->nullable();
             $table->timestamps();
             $table->softDeletes();
-            $table->integer('created_by')->default(0);
-            $table->integer('updated_by')->default(0);
-            $table->integer('deleted_by')->default(0);
 
-            $table->index('m_user_roles_id');
             $table->index('email');
-            $table->index('name');
+            $table->index('username');
             $table->index('updated_security');
         });
     }
@@ -52,6 +42,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('m_user');
+        Schema::dropIfExists('user');
     }
 };
